fd5b01bffcd0daad6985333ed027e9adc1739dbd
Dant86 <vedantdpathak@iCloud.com>
2019-11-25 10:43:52 -0600

    BOW model works better than conceptnet...

--- classify.py
+++ classify.py
@@ -2,7 +2,9 @@ from pandas import read_csv
 from conceptnet5.vectors.query import VectorSpaceWrapper
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from wordfreq import simple_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import numpy as np
 import torch
 import torch.autograd as auto
@@ -11,60 +13,60 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 # some globals
+STOP_WORDS = set(stopwords.words('english'))
 PATH_TO_MINI = '../mini.h5'
 TRAIN_SIZE = 0.7
-EPOCHS = 10000
+EPOCHS = 4000
 
 # init wrapper and load data
 df = read_csv('all.csv')
 labels = list(df.columns)[4:]
+
 df[labels] = df[labels] * 1
 wrapper = VectorSpaceWrapper(PATH_TO_MINI)
+vectorizer = TfidfVectorizer()
+vectorizer.fit(df['desc'])
 
 # create + format train and test data
 train, test = train_test_split(df, train_size=TRAIN_SIZE, test_size=None)
 
 mean = lambda l: sum(l) / len(l)
 
-
 def desc2vec(desc):
-    tokenized = simple_tokenize(desc)
+    tokenized = word_tokenize(desc)
+    tokenized = [w for w in tokenized if w not in STOP_WORDS]
     vecs = list(map(lambda s: wrapper.text_to_vector('en', s), tokenized))
     return mean(vecs)
 
-
 train_xs = np.array(train['desc'].map(desc2vec))
+train_xs_with_tfidf = vectorizer.transform(train['desc']).todense()
+vocab_size = train_xs_with_tfidf.shape[1]
 train_ys = np.array(train[labels])
 test_xs = np.array(test['desc'].map(desc2vec))
+test_xs_with_tfidf = vectorizer.transform(test['desc']).todense()
 test_ys = np.array(test[labels])
 
 # batch training data
 batched_xs = []
-batched_ys= []
+batched_ys = []
 for i in range(0, len(train_xs), 50):
-    x = train_xs[i:i + 50]
+    x = train_xs_with_tfidf[i:i + 50]
     batched_xs.append(np.stack(x))
     y = train_ys[i:i + 50]
     batched_ys.append(np.stack(y))
 amt_batches = len(batched_xs)
-
 
 # define neural network structure and forward pass
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.w1 = nn.Linear(300, 50)
-        self.w2 = nn.Linear(50, 50)
-        self.w3 = nn.Linear(50, 10)
-        self.w4 = nn.Linear(10, 2)
+        self.w1 = nn.Linear(vocab_size, 10)
+        self.w2 = nn.Linear(10, 2)
 
     def forward(self, x):
         x = F.relu(self.w1(x))
-        x = F.relu(self.w2(x))
-        x = F.relu(self.w3(x))
-        return F.log_softmax(self.w4(x))
-
+        return F.log_softmax(self.w2(x))
 
 # define one model for each label -- perhaps this might work nicely
 classifiers = [Net() for label in labels]
@@ -85,7 +87,7 @@ for i, clf in enumerate(classifiers):
         for xb, yb in zip(batched_xs, batched_ys):
             yb = np.array([l[i] for l in yb])
             adam.zero_grad()
-            preds = clf(to_torch(xb))
+            preds = clf(to_torch(xb).float())
             loss = cost(preds, to_torch(yb))
             loss.backward()
             adam.step()
@@ -94,13 +96,18 @@ for i, clf in enumerate(classifiers):
             final_loss = total_loss / amt_batches
     print(f'Loss for {labels[i]}: {final_loss}')
 
-
 def make_prediction(x, clfs):
-    return np.array([int(clf(to_torch(x)).argmax()) for clf in clfs])
+    return np.array([int(clf(to_torch(x).float()).argmax()) for clf in clfs])
 
+def save_model(clfs):
+    for i, clf in enumerate(clfs):
+        torch.save(clf.state_dict(), f'models/MODEL_{i}.pt')
+
+save_model(classifiers)
 
 # validation step
 test_preds = np.stack(list(map(lambda x: make_prediction(x, classifiers),
-                               test_xs)))
+                               test_xs_with_tfidf)))
 acc = accuracy_score(test_preds, test_ys)
 print(f'Test accuracy: {acc * 100}%')
+
