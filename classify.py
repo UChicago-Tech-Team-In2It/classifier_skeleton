db538e68c7d041381727f77858f0355941d95ee1
Dant86 <vedantdpathak@iCloud.com>
2019-12-19 17:12:21 +0000

    working classifier

--- classify.py
+++ classify.py
@@ -5,53 +5,49 @@ from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from warnings import filterwarnings
+from tqdm import tqdm
 import numpy as np
 import torch
 import torch.autograd as auto
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import pickle
+
+filterwarnings('ignore')
 
 # some globals
 STOP_WORDS = set(stopwords.words('english'))
 PATH_TO_MINI = '../mini.h5'
 TRAIN_SIZE = 0.7
-EPOCHS = 10000
+EPOCHS = 1000
 
 # init wrapper and load data
 df = read_csv('volunteermatch_data.csv').dropna()
-print(df.head())
 labels = list(df.columns)[5:]
 
 df[labels] = df[labels] * 1
-wrapper = VectorSpaceWrapper(PATH_TO_MINI)
-vectorizer = TfidfVectorizer()
-vectorizer.fit(df['desc'])
 
 # create + format train and test data
 train, test = train_test_split(df, train_size=TRAIN_SIZE, test_size=None)
 
-mean = lambda l: sum(l) / len(l)
+vectorizer = TfidfVectorizer()
+vectorizer.fit(train['desc'])
+with open('vectorizer.pickle', 'wb') as pkl_f:
+    pickle.dump(vectorizer, pkl_f)
 
-def desc2vec(desc):
-    tokenized = word_tokenize(desc)
-    tokenized = [w for w in tokenized if w not in STOP_WORDS]
-    vecs = list(map(lambda s: wrapper.text_to_vector('en', s), tokenized))
-    return mean(vecs)
-
-train_xs = np.array(train['desc'].map(desc2vec))
-train_xs_with_tfidf = vectorizer.transform(train['desc']).todense()
-vocab_size = train_xs_with_tfidf.shape[1]
+train_xs = vectorizer.transform(train['desc']).todense()
+vocab_size = train_xs.shape[1]
 train_ys = np.array(train[labels])
-test_xs = np.array(test['desc'].map(desc2vec))
-test_xs_with_tfidf = vectorizer.transform(test['desc']).todense()
+test_xs = vectorizer.transform(test['desc']).todense()
 test_ys = np.array(test[labels])
 
 # batch training data
 batched_xs = []
 batched_ys = []
 for i in range(0, len(train_xs), 50):
-    x = train_xs_with_tfidf[i:i + 50]
+    x = train_xs[i:i + 50]
     batched_xs.append(np.stack(x))
     y = train_ys[i:i + 50]
     batched_ys.append(np.stack(y))
@@ -62,12 +58,14 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.w1 = nn.Linear(vocab_size, 10)
-        self.w2 = nn.Linear(10, 2)
+        self.w1 = nn.Linear(vocab_size, 1000)
+        self.w2 = nn.Linear(1000, 100)
+        self.w3 = nn.Linear(100, 2)
 
     def forward(self, x):
         x = F.relu(self.w1(x))
-        return F.log_softmax(self.w2(x))
+        x = F.relu(self.w2(x))
+        return F.log_softmax(self.w3(x))
 
 # define one model for each label -- perhaps this might work nicely
 classifiers = [Net() for label in labels]
@@ -83,7 +81,7 @@ for i, clf in enumerate(classifiers):
     print(f'Training model on {labels[i]}...')
     adam = adams[i]
     final_loss = 0
-    for epoch in range(EPOCHS):
+    for epoch in tqdm(range(EPOCHS)):
         total_loss = 0.0
         for xb, yb in zip(batched_xs, batched_ys):
             yb = np.array([l[i] for l in yb])
@@ -108,7 +106,7 @@ save_model(classifiers)
 
 # validation step
 test_preds = np.stack(list(map(lambda x: make_prediction(x, classifiers),
-                               test_xs_with_tfidf)))
+                               test_xs)))
 acc = accuracy_score(test_preds, test_ys)
 print(f'Test accuracy: {acc * 100}%')
 
